12f2b728e53dfd7c2d24c221f84eddfce7b4c57e
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2026-04-26 18:27:47 +0530

    Lecture 13

---
+++ Lecture 13/002Functions_ModifyArray.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+void modify(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		arr[i]++;
+	}
+}
+
+int c = 1;
+
+int& f() {
+	return c;
+}
+
+int main() {
+
+	int arr[] = {10, 20, 30, 40, 50};
+	int n = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	modify(arr, n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
+	f() = c++;
+
+	cout << c << endl;
+
+	return 0;
+}
