fff60c68efd027b50d183e008a1d22f3a7c70c91
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2026-05-30 18:31:50 +0530

    Lecture 16

---
+++ Lecture 19/004Pointers_Dereferencing.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int x = 10;
+	int *xptr = &x;
+
+	cout << "x = " << x << endl;
+	cout << "sizeof(x) = " << sizeof(x) << endl;
+	cout << "xptr = " << xptr << endl;
+	cout << "sizeof(xptr) = " << sizeof(xptr) << endl;
+	cout << "*xptr = " << *xptr << endl << endl;
+
+	double y = 3.14;
+	double* yptr = &y;
+
+	cout << "y = " << y << endl;
+	cout << "sizeof(y) = " << sizeof(y) << endl;
+	cout << "yptr = " << yptr << endl;
+	cout << "sizeof(yptr) = " << sizeof(yptr) << endl;
+	cout << "*yptr = " << *yptr << endl << endl;
+
+
+
+	return 0;
+}
